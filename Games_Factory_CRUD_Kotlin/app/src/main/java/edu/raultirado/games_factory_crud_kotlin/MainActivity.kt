ff3769b7bde4d771b3cd1d084aa759e3ab1e7321
rault <rault@LAPTOP-DDM3M1EB>
2025-12-17 15:55:09 +0100

    Hola Frank

--- Games_Factory_CRUD_Kotlin/app/src/main/java/edu/raultirado/games_factory_crud_kotlin/MainActivity.kt
+++ Games_Factory_CRUD_Kotlin/app/src/main/java/edu/raultirado/games_factory_crud_kotlin/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hola $name!",
+        text = "Hola Frank $name!",
         modifier = modifier
     )
 }
