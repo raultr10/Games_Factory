32c48024282024367f7fa4be9e226b8db92736c4
rault <rault@LAPTOP-DDM3M1EB>
2025-12-17 15:47:00 +0100

    Proyecto para la aplicacion de movil

---
+++ Games_Factory_CRUD_Kotlin/app/src/main/java/edu/raultirado/games_factory_crud_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.raultirado.games_factory_crud_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.raultirado.games_factory_crud_kotlin.ui.theme.Games_Factory_CRUD_KotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Games_Factory_CRUD_KotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Games_Factory_CRUD_KotlinTheme {
+        Greeting("Android")
+    }
+}
