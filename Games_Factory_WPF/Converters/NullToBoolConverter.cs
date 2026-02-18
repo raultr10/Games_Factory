2377cf8ad5fd5a838f92cc2ef9b0033eca24744c
FrankSC666 <frankxixona@gmail.com>
2026-02-18 22:51:43 +0100

    Creación de las funcionalidades de conversión.

---
+++ Games_Factory_WPF/Converters/NullToBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games_Factory.Converters
+{
+    public class NullToBoolConverter
+    {
+        // Devuelvo el true si el valor no es nulo.
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
