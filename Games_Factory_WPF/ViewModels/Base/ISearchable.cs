fe2c44da29843e985ce3a3048e98fbaaa32dc876
FrankSC666 <frankxixona@gmail.com>
2026-02-18 23:00:15 +0100

    Implementación de las funcionalidades base de los ViewModels.

---
+++ Games_Factory_WPF/ViewModels/Base/ISearchable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games_Factory.ViewModels.Base
+{
+    public interface ISearchable
+    {
+        String SearchText { get; set; }
+    }
+}
---
+++ Games_Factory_WPF/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Games_Factory.ViewModels.Base
+{
+    // Implemento la interfaz ICommand para manejar las acciones en las vistas.
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?>? _canExecute;
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
+        public void Execute(object? parameter) => _execute(parameter);
+    }
+}
